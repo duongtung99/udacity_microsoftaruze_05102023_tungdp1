4a482a1bac2f8af55e39b1847a3a10dcc941540d
VietAnh <anguyen@zuelligpharma.com>
2023-10-06 09:47:11 +0700

    add capture login success login failed

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,16 +66,15 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.info('login failed: Invalid username or password, {}'.format(form.username.data))
             flash('Invalid username or password')
-            app.logger.info(form.username.data + ' Login failed!')
             return redirect(url_for('login'))
+        app.logger.info('Admin login success!')
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-            app.logger.info('Admin login success!')
         return redirect(next_page)
-    app.logger.info('Admin login success!')
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
